0326ab7d5ff684555424bcb09272ba123d7bef52
Hyunjun Yoon <yhj940719@gmail.com>
2021-09-28 21:44:37 -0400

    image crawling on a browser

--- index.py
+++ index.py
@@ -1,1 +1,7 @@
-print("Hello world")
+from bs4 import BeautifulSoup
+from urllib.request import urlopen
+
+response= urlopen('https://www.naver.com') 
+soup = BeautifulSoup(response, 'html.parser')
+for anchor in soup.select("span.blind"):
+    print(anchor.get_text())
