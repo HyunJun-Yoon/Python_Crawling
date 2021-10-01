b06fd2665a424f7783bf349c80924308dcd5159f
Hyunjun Yoon <yhj940719@gmail.com>
2021-09-30 23:02:21 -0400

    animal-face added

--- selenium/google.py
+++ selenium/google.py
@@ -24,7 +24,7 @@ while True:
 images = driver.find_elements_by_css_selector(".rg_i.Q4LuWd")
 count = 1
 for image in images:
-    try:
+    try: 
         image.click()
         time.sleep(3)
         imgUrl = driver.find_element_by_xpath('/html/body/div[2]/c-wiz/div[3]/div[2]/div[3]/div/div/div[3]/div[2]/c-wiz/div/div[1]/div[1]/div[2]/div/a/img').get_attribute("src")
